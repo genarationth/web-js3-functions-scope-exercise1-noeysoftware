8872c033d3fad4e6f29ca5e567ae4d27f5b021bb
noeysoftware <noeysoftware@gmail.com>
2024-05-14 13:59:02 +0700

    finished exercise1

--- exercise1.js
+++ exercise1.js
@@ -1,1 +1,40 @@
-console.log("hehehehehe");
+function add(a, b) {
+  let add = a + b;
+  return add;
+}
+
+console.log(add(1, 2));
+
+function subtract(a, b) {
+  let subtract = a - b;
+  return subtract;
+}
+
+console.log(subtract(1, 2));
+
+function multiply(a, b) {
+  let multiply = a * b;
+  return multiply;
+}
+
+console.log(multiply(1, 2));
+
+function divide(a, b) {
+  let divide = a / b;
+  return divide;
+}
+
+console.log(divide(1, 2));
+
+function inform(firstName, location, hobby) {
+  console.log(
+    "Hi, my name is " +
+      firstName +
+      " I live in " +
+      location +
+      " and enjoy " +
+      hobby
+  );
+}
+
+inform("the king", "germany", "riding");
